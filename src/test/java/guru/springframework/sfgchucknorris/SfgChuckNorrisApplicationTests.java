2acf71efe9d7820bb84e843c449c6ae120b39637
Akshar <akshar.sivnarain@gmail.com>
2022-08-29 08:45:22 +0200

    Added dependancy

---
+++ src/test/java/guru/springframework/sfgchucknorris/SfgChuckNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgchucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgChuckNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
